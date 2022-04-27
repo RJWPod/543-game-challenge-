3298f9c99d09aba97844d01e001fc4771606a9df
RJWPod <101339801+RJWPod@users.noreply.github.com>
2022-04-27 08:50:57 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    Player_Variable += 1
+    if (Player_Variable == 1) {
+    	
+    }
+    while (true) {
+        led.unplot(0, 0)
+        basic.pause(10)
+        led.plot(0, 0)
+    }
+})
 let Player_Variable = 0
+Player_Variable = 0
 for (let X = 0; X <= 4; X++) {
     for (let Y = 0; Y <= 4; Y++) {
         led.plot(X - Y, Y)
